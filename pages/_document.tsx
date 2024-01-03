5045b2580be3f4b55ace67b3ca3a46714cd82b71
sshmatrix <cryptosauron@protonmail.com>
2024-01-03 10:01:34 +0530

    rSync

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,9 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/images/favicon.png" />
         <link rel="preload" href="https://fonts.googleapis.com/icon?family=Material+Icons" as="style" />
-        <link rel="preload" href="SF-Mono.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-        <link rel="preload" href="Spotnik.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-        <link rel="preload" href="Rajdhani.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="fonts/SF-Mono.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="fonts/Spotnik.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="fonts/Bioliquid.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
